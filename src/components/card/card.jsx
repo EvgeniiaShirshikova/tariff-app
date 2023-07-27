ff1d3a6613ed5a21e0f3cd359495f98239ff7ffd
EvgeniiaShirshikova <shirshikovae@gmail.com>
2023-07-28 01:19:07 +0300

    app

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,12 +1,13 @@
 import st from "./style.module.scss"
 
-export default function Card() {
+export default function Card(props) {
+    const {name,price,speed,description} = props
     return (
         <div className={st.container}>
-            <div className={st.name}>name</div>
-            <div className={st.price}>price</div>
-            <div className={st.volume}>volume</div>
-            <div className={st.description}>description</div>
+            <div className={st.name}>{name}</div>
+            <div className={st.price}>{price}</div>
+            <div className={st.speed}>{speed}</div>
+            <div className={st.description}>{description}</div>
         </div>
     )
 }
--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Card from "../card/card";
 import st from "./style.module.scss"
+import tariffs from "../../data/tariffs.json"
 
 export default function Cards() {
     return (
         <div className={st.container}>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
+        {tariffs.map((item,index)=>(
+            <Card key={index} name={item.name} price={item.price} speed={item.speed} description={item.description}/>
+        ))}
         </div>
     )
 }
