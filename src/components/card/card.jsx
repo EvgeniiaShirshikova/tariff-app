53104cb14f808e4092b0d359b67f6fbc48d19714
EvgeniiaShirshikova <shirshikovae@gmail.com>
2023-07-28 02:03:45 +0300

    card

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,11 +1,11 @@
 import st from "./style.module.scss"
 
 export default function Card(props) {
-    const {name,price,speed,description} = props
+    const {name,price,speed,description,name_bgcolor,price_bgcolor} = props
     return (
-        <div className={st.container}>
-            <div className={st.name}>{name}</div>
-            <div className={st.price}>{price}</div>
+        <div className={st.card}>
+            <div className={`${st.name} ${st[name_bgcolor]}`}>{name}</div>
+            <div className={`${st.price} ${st[price_bgcolor]}`}>{price}</div>
             <div className={st.speed}>{speed}</div>
             <div className={st.description}>{description}</div>
         </div>
