24946b9f378e0489ec59f25812873e0e8d3dba44
EvgeniiaShirshikova <shirshikovae@gmail.com>
2023-07-28 05:58:52 +0300

    app is ready

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -7,7 +7,7 @@ export default function Cards() {
     return (
         <div className={st.container}>
         {tariffs.map((item,index)=>(
-            <Card key={index} name={item.name} price={item.price} speed={item.speed} description={item.description} name_bgcolor={item.name_bgcolor} price_bgcolor={item.price_bgcolor}/>
+            <Card key={index} name={item.name} price={item.price} speed={item.speed} description={item.description} name_bgcolor={item.name_bgcolor} price_bgcolor={item.price_bgcolor} isSelected={item.isSelected}/>
         ))}
         </div>
     )
