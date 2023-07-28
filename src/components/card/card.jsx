ecf1a34e648090875ccb0d54bb9cdcc50df7a5fb
EvgeniiaShirshikova <shirshikovae@gmail.com>
2023-07-28 06:42:31 +0300

    app and readme

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -3,7 +3,7 @@ import st from "./style.module.scss"
 export default function Card(props) {
     const {name,price,speed,description,name_bgcolor,price_bgcolor,isSelected} = props
     return (
-        <div className={`${st.card} ${(isSelected ? st.selected : "")}`}>
+        <div className={`${st.card} ${(isSelected && st.selected)}`}>
             <div className={`${st.name} ${st[name_bgcolor]}`}>{name}</div>
             <div className={`${st.price} ${st[price_bgcolor]}`}>{price}</div>
             <div className={st.speed}>{speed}</div>
